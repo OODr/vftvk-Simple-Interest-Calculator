e9523cf81fbb3e5d6c770ac4f9a77649123e7182
OODr <93932136+OODr@users.noreply.github.com>
2021-11-11 13:44:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -26,4 +26,4 @@ function compute()
         document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
     }*/
 }
-document.getElementById("compute-int").addEventListener("click", compute());      
+document.getElementById("compute-int").addEventListener("click", compute);      
