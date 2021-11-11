455335fc15a1e85f191464283e655f6d52db94e4
OODr <93932136+OODr@users.noreply.github.com>
2021-11-11 15:19:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-/*Variables for user input and script caculations */
-var principal = document.getElementById("principal").value;
-var rate = document.getElementById("rate").value;
-var years = document.getElementById("years").value;
-var interest = principal * years * rate /100;
-var year = new Date().getFullYear()+parseInt(years);
+
 
 /*Update rates with real time user changes */
 function updateRate() 
@@ -14,16 +9,20 @@ function updateRate()
 /*Script for the Compute Interest Button function*/
 function compute()
 {
-    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
-    /*Make sure principal values are proper
+    /*Variables for user input and script caculations */
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+    /*Make sure principal values are proper*/
     if (principal <= 0) {
         alert("Enter a positive number");
         document.getElementById('principal').style.borderColor = "red";
         document.getElementById('principal').focus();
-    }*/
-    /*Print the results of the inquiery
+    }
+    /*Print the results of the inquiery*/
     else if(principal > 0) {
         document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
-    }*/
-}
-document.getElementById("compute-int").addEventListener("click", compute);      
+    }
+} 
