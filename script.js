7a1edaf10ee06ceb420c45f459f54d870d9c2b59
OODr <93932136+OODr@users.noreply.github.com>
2021-11-11 11:37:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -14,15 +14,16 @@ function updateRate()
 /*Script for the Compute Interest Button function*/
 function compute()
 {
-    /*Make sure principal values are proper*/
+    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+    /*Make sure principal values are proper
     if (principal <= 0) {
         alert("Enter a positive number");
         document.getElementById('principal').style.borderColor = "red";
         document.getElementById('principal').focus();
-    }
-    /*Print the results of the inquiery*/
+    }*/
+    /*Print the results of the inquiery
     else if(principal > 0) {
         document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
-    }
+    }*/
 }
       
